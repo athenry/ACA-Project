4f0f09d6de4103fb6dcc2a071b07cc42ed8c15dc
Alison Henry <alisontaylorhenry@gmail.com>
2018-10-30 14:55:13 -0600

    added geo locations for Unknown and blank Coded.locations (both on UofA campus)

--- ACA code scraps.R
+++ ACA code scraps.R
@@ -3,3 +3,7 @@
 authorList$First.initial.s. <- str_remove_all(authorList$First.initial.s., "[.]")
 
 authorList$Middle.initial.s. <- str_remove_all(authorList$Middle.initial.s., "[.]")
+
+## map1 <- canada +
+##    geom_point(aes(x=lng, y=lat), 
+##               data = grantsperLoc)
