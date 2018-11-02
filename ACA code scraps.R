878c8b8d7c55ea48ab22b1381fa506bcb4c23672
Alison Henry <alisontaylorhenry@gmail.com>
2018-11-02 15:41:46 -0600

    missed file

--- ACA code scraps.R
+++ ACA code scraps.R
@@ -7,3 +7,10 @@ authorList$Middle.initial.s. <- str_remove_all(authorList$Middle.initial.s., "[.
 ## map1 <- canada +
 ##    geom_point(aes(x=lng, y=lat), 
 ##               data = grantsperLoc)
+
+## canada <- ggplot() + borders(database = "worldHires", "Canada", colour = "gray80", fill = "gray85") + theme_map()
+
+## map1 <- canada +
+##geom_point(aes(x=lon, y=lat), 
+##           data = grantsperLoc,
+##           colour = 'green', alpha = .5)
